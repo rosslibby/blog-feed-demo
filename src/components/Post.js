916205af0b27d0f960ce3aa4ace246975a971a0f
Ross Libby <ross@frvctl.com>
2021-04-14 09:50:28 -0500

    add font awesome, comment icon, capitalize title, capitalize first letter of each post

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,6 +6,9 @@ const Post = ({ title, body, id, userId }) => {
   <div className="post">
     <h2 className="post__title">{title}</h2>
     <p className="post__body">{body}</p>
+    <div className="post__metadata">
+      <i className="fal fa-comment-alt-lines" />
+    </div>
   </div>
 )}
 
